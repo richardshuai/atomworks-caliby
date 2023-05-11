8bd831844fd525477307f207dd671c2af2d36c06
Rohith Krishna <rohith@digs.ipd.uw.edu>
2023-05-11 15:15:01 -0700

    first commit of cifutils in separate dir

---
+++ cifutils/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='CIFUtils: python package for reading mmCIF files from the PDB',
+    version='1.0',
+    packages=find_packages(),
+)
+
