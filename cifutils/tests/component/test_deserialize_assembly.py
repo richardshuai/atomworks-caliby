3101a77871f3287aaf9e8ada3863e5b6b36c0cfe
Simon Mathis <smathis@uw.edu>
2024-06-03 20:14:00 -0700

    fix: update deserialization checks

--- cifutils/tests/component/test_deserialize_assembly.py
+++ cifutils/tests/component/test_deserialize_assembly.py
@@ -1,63 +1,19 @@
 import pytest
 from tests.conftest import get_digs_path
-from cifutils.cifutils_biotite import cifutils_biotite, cifutils_biotite_utils
+from cifutils.cifutils_biotite import cifutils_biotite
 
 TEST_CASES = [
+    # With the wrong version of biotite, these will lead to cif deserialization errors as the assembly category is represented slightly differently in these files
     "5xa9",
     "5xag",
     "5xaf",
-    "5xa7",
-    "3qh0",
-    "7qh6",
-    "7qh4",
-    "4qht",
-    "1qh8",
-    "6vo3",
-    "7jsx",
-    "5jsi",
-    "6mti",
-    "4mtl",
-    "1s3q",
-    "8czo",
-    "6xkk",
-    "2xkq",
-    "4xku",
-    "6xkl",
-    "6vec",
-    "7qbf",
-    "6s91",
-    "6jy3",
-    "6jy4",
-    "5jyg",
-    "7dwz",
-    "1dwk",
-    "5dws",
-    "6dwj",
-    "5dwy",
-    "5cpz",
-    "3h6o",
-    "5xa9",
-    "5xag",
-    "5xaf",
-    "5xa7",
-    "3qh0",
-    "7qh6",
-    "7qh4",
-    "4qht",
-    "1qh8",
-    "6vo3",
+    "5ocm",
 ]
 
 cif_parser = cifutils_biotite.CIFParser()
-# Manual debug
-pdb_id = TEST_CASES[0]
-cif = cifutils_biotite_utils.read_cif_file(get_digs_path(pdb_id))
-cifutils_biotite_utils.category_to_dict(cif.block, "pdbx_struct_assembly")
-cifutils_biotite_utils.category_to_dict(cif.block, "pdbx_struct_assembly_gen")
-cifutils_biotite_utils.category_to_dict(cif.block, "pdbx_struct_oper_list")
 
 
-@pytest.mark.parametrize("assembly_id", TEST_CASES)
+@pytest.mark.parametrize("pdb_id", TEST_CASES)
 def test_deserialize_assembly(pdb_id: str):
     digs_path = get_digs_path(pdb_id)
     result = cif_parser.parse(digs_path, build_assembly="all")
