61ae5a5f58ab2c39f1f7d1c350aa3bea6ddea7b3
Simon Mathis <smathis@uw.edu>
2024-06-06 16:06:17 -0700

    feat: add arginine renaming tests

---
+++ cifutils/tests/component/test_arginine_renaming.py
@@ -0,0 +1,12 @@
+from tests.conftest import get_digs_path, TEST_DATA_DIR, assert_same_atom_array, CIF_PARSER
+
+
+def test_arginine_renaming():
+    correct_file = get_digs_path("101m")
+    renamed_file = TEST_DATA_DIR / "101m_arginine_nh1nh2_swapped.cif"
+    result1 = CIF_PARSER.parse(correct_file, build_assembly=None)
+    result2 = CIF_PARSER.parse(renamed_file, build_assembly=None)
+    assert_same_atom_array(result1["atom_array"], result2["atom_array"])
+
+
+test_arginine_renaming()
