57166f5d2e0f799ac3aa10ab9df917314035268c
Simon Mathis <smathis@uw.edu>
2024-09-04 04:49:56 +0000

    fix: add test coverage bit

--- cifutils/.ci/run_tests.sh
+++ cifutils/.ci/run_tests.sh
@@ -1,1 +1,3 @@
-/projects/ml/RF2_allatom/spec_files/cifutils_latest.sif pytest tests
+/projects/ml/RF2_allatom/spec_files/cifutils_latest.sif pytest --cov=cifutils --cov-report=term-missing tests/
+# Output the coverage in a format GitLab can parse
+/projects/ml/RF2_allatom/spec_files/cifutils_latest.sif coverage report | tail -n 1 | awk '{print "TOTAL", $NF}'
