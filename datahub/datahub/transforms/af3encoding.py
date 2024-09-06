b7717333ba016a1774f132a8565c56e33401a90d
Simon Mathis <smathis@uw.edu>
2024-09-05 17:17:11 -0700

    clean: cleanup dev code

--- datahub/datahub/transforms/af3encoding.py
+++ datahub/datahub/transforms/af3encoding.py
@@ -2,64 +2,29 @@ from itertools import cycle
 
 import biotite.structure as struc
 import numpy as np
-from cifutils.utils.selection_utils import annot_start_stop_idxs
+from cifutils.constants import STANDARD_AA, STANDARD_DNA, STANDARD_RNA
 
 from datahub.encoding_definitions import AA_LIKE_CHEM_TYPES, DNA_LIKE_CHEM_TYPES, RNA_LIKE_CHEM_TYPES
 from datahub.transforms.atom_array import (
     add_global_token_id_annotation,
-    get_within_entity_idx,
-    get_within_group_res_idx,
 )
-from datahub.transforms.atomize import atomize_residues
 from datahub.utils.token import get_token_starts
 
-STANDARD_AA = [
-    "ALA",
-    "ARG",
-    "ASN",
-    "ASP",
-    "CYS",
-    "GLN",
-    "GLU",
-    "GLY",
-    "HIS",
-    "ILE",
-    "LEU",
-    "LYS",
-    "MET",
-    "PHE",
-    "PRO",
-    "SER",
-    "THR",
-    "TRP",
-    "TYR",
-    "VAL",
-]
-"""The 20 standard amino acids"""
-
-STANDARD_RNA = ["A", "C", "G", "U"]
-"""The 4 standard RNA nucleotides"""
-
-STANDARD_DNA = ["DA", "DC", "DG", "DT"]
-"""The 4 standard DNA nucleotides"""
-
+# fmt: off
 AF3_TOKENS = (
     # 20 AA + 1 unknown AA
-    STANDARD_AA
-    + ["UNK"]
+    list(STANDARD_AA) + ["UNK"]
     +
     # 4 RNA + 1 unknown RNA
-    STANDARD_RNA
-    + ["X"]
+    list(STANDARD_RNA) + ["X"]
     +
     # 4 DNA + 1 unknown DNA
-    STANDARD_DNA
-    + ["DX"]
-    +
+    list(STANDARD_DNA) + ["DX"]
     # 1 gap
-    ["<G>"]
+    + ["<G>"]
 )
 """Sequence tokens in AF3"""
+# fmt: on
 
 
 # Build up chemcomp type mappings
@@ -81,7 +46,8 @@ token_to_int = {token: i for i, token in enumerate(AF3_TOKENS)}
 to_af3_int = np.vectorize(lambda x: token_to_int.get(x, token_to_int["UNK"]))
 
 
-def encode_atom_names_like_af3(atom_names):
+def encode_atom_names_like_af3(atom_names: np.ndarray) -> np.ndarray:
+    """Encodes atom names like AF3"""
     # Ensure uppercase
     atom_names = np.char.upper(atom_names)
     # Turn into 4 character ASCII string (this truncates longer atom names)
@@ -102,71 +68,3 @@ def get_atom_to_token_idx(atom_array):
 
 def get_token_to_atom_idx(atom_array):
     return get_token_starts(atom_array)
-
-
-atom_array = atomize_residues(
-    atom_array,
-    atomize_by_default=True,
-    res_names_to_ignore=STANDARD_AA + STANDARD_RNA + STANDARD_DNA,
-    move_atomized_part_to_end=False,
-    validate_atomize=False,
-)
-
-# SET 1 (token level)
-# ... get token-level array
-token_starts = get_token_starts(atom_array)
-token_level_array = atom_array[token_starts]
-# identifier tokens
-residue_index = get_within_group_res_idx(token_level_array, group_by="chain_iid")
-token_index = np.arange(len(token_starts))
-asym_name, asym_id = np.unique(token_level_array.chain_iid, return_inverse=True)
-entity_name, entity_id = np.unique(token_level_array.chain_entity, return_inverse=True)
-# TODO: Sym ID is not quite right yet ...
-sym_name, sym_id = get_within_entity_idx(token_level_array, level="chain")
-# sequence tokens
-restype = to_af3_int(token_level_array.res_name)
-# molecule type
-is_protein = np.isin(token_level_array.res_name, _all_res_names[_is_aa_like])
-is_rna = np.isin(token_level_array.res_name, _all_res_names[_is_rna_like])
-is_dna = np.isin(token_level_array.res_name, _all_res_names[_is_dna_like])
-is_ligand = ~(is_protein | is_rna | is_dna)
-
-# SET 2 (atom level)
-# TODO: ref_pos (get reference conformers for each CCD code or SMILES
-# TODO: ref_mask
-# Encode element
-ref_element = atom_array.element.astype(int)
-# Charge
-ref_charge = atom_array.charge
-# atom name
-ref_atom_name_chars = encode_atom_names_like_af3(atom_array.atom_name)
-# ref_space_uid
-ref_space_uid = add_global_token_id_annotation(atom_array).token_id
-
-# SET 3 (MSA level)
-# -> Nate
-
-# SET 4 (template level)
-
-# SET 5 (bond level)
-# TODO:
-
-
-annot_start_stop_idxs(atom_array, ["chain_iid"], add_exclusive_stop=False)
-
-
-# 1. Crop full atom array
-#  - We need to get conformers for things that:
-#     - is_polymer & token_in_crop
-#     - (~is_polymer) & token_in_crop
-#  (Decision: treat covalently bonded ligands differently?)
-#    i.e. get a conformer for the covalently bonded ligand or
-#         only get a conformer for the residues
-
-# 2. Generate the conformers (residue level)
-#  - Conformer pos & conformer mask
-
-# 3. Encode sequence, element, atom_name, res_iid
-# (DONE)
-
-# def get_af3_reference()
