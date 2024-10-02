ba86743b5c30565161d072ba8daed542dd58664d
Rohith Krishna <rohith@localhost>
2024-10-02 12:23:18 -0700

    add ability to batch structures

--- datahub/datahub/transforms/batch_structures.py
+++ datahub/datahub/transforms/batch_structures.py
@@ -13,11 +13,11 @@ class BatchStructures(Transform):
     def check_input(self, data):
         check_contains_keys(data, ["ground_truth", "atom_array"])
         check_contains_keys(data["ground_truth"], ["coord_atom_lvl", "mask_atom_lvl"])
-        assert len(data["ground_truth"]["coord_atom_lvl"]) == len(atom_array), "structure must not be batched yet"
-        assert len(data["ground_truth"]["mask_atom_lvl"]) == len(atom_array), "mask must not be batched yet"
+        assert len(data["ground_truth"]["coord_atom_lvl"]) == len(data["atom_array"]), "structure must not be batched yet"
+        assert len(data["ground_truth"]["mask_atom_lvl"]) == len(data["atom_array"]), "mask must not be batched yet"
 
     def forward(self, data):
-        data["ground_truth"]["coord_atom_lvl"] = data["ground_truth"]["coord_atom_lvl"][None].repeat(self.batch_size, 1, 1, 1)
-        data["ground_truth"]["mask_atom_lvl"] = data["ground_truth"]["mask_atom_lvl"][None].repeat(self.batch_size, 1, 1)
+        data["ground_truth"]["coord_atom_lvl"] = data["ground_truth"]["coord_atom_lvl"].repeat(self.batch_size, 1, 1)
+        data["ground_truth"]["mask_atom_lvl"] = data["ground_truth"]["mask_atom_lvl"].repeat(self.batch_size, 1)
         return data
 
