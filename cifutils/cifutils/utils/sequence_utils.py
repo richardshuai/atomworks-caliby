b7f4af79a89c952a6183b5b27c6416cf365b7d72
Rohith Krishna <rohith@localhost>
2024-10-02 14:32:36 -0700

    add ability to delineate purines and pyrimidines

--- cifutils/cifutils/utils/sequence_utils.py
+++ cifutils/cifutils/utils/sequence_utils.py
@@ -5,6 +5,8 @@ __all__ = [
     "get_3_from_1_letter_code",
 ]
 
+import numpy as np
+
 from cifutils.utils.io_utils import logger
 from cifutils.enums import ChainType
 from Bio.Data.PDBData import (
@@ -115,3 +117,32 @@ def get_3_from_1_letter_code(
     else:
         logger.error(f"Unsupported chain type: {chain_type}, returning unknown protein residue (i.e., UNK).")
         return unknown_protein_three_letter
+
+
+def is_pyramidine(res_names: list) -> np.array:
+    """
+    Given a list of 3-letter residue names, returns a boolean array indicating whether each residue is a pyramidine.
+    """
+    def is_pyramidine_residue(res_name: str) -> bool:
+        return res_name in ["C", "U", "DC", "DT"]
+    apply = np.vectorize(is_pyramidine_residue)
+    return apply(np.array(res_names))
+
+
+def is_purine(res_names: list) -> np.array:
+    """
+    Given a list of 3-letter residue names, returns a boolean array indicating whether each residue is a purine.
+    """
+    def is_purine_residue(res_name: str) -> bool:
+        return res_name in ["A", "G", "DA", "DG"]
+    apply = np.vectorize(is_purine_residue)
+    return apply(np.array(res_names))
+
+def is_unknown_nucleotide(res_names: list) -> np.array:
+    """
+    Given a list of 3-letter residue names, returns a boolean array indicating whether each residue is an unknown nucleotide.
+    """
+    def is_unknown_nucleotide_residue(res_name: str) -> bool:
+        return res_name in ["X", "DX"]
+    apply = np.vectorize(is_unknown_nucleotide_residue)
+    return apply(np.array(res_names))
---
+++ cifutils/tests/components/test_sequence_utils.py
@@ -0,0 +1,127 @@
+import pytest
+import numpy as np
+
+from cifutils.utils.sequence_utils import is_pyramidine, is_purine, is_unknown_nucleotide
+
+def test_is_pyramidine():
+    sequence = [
+        "MET",
+        "LEU",
+        "GLY",
+        "VAL",
+        "ALA",
+        "DA",
+        "DC", 
+        "DG",
+        "DT", 
+        "UNK",
+        "A",
+        "C", 
+        "G",
+        "U", 
+        "DX",
+        "X",
+    ]
+
+    expected = [
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        True,
+        False,
+        True,
+        False,
+        False,
+        True,
+        False,
+        True,
+        False,
+        False,
+    ]
+    assert (is_pyramidine(sequence) == np.array(expected)).all()
+
+def test_is_purine():
+    
+    sequence = [
+        "MET",
+        "LEU",
+        "GLY",
+        "VAL",
+        "ALA",
+        "DA", 
+        "DC", 
+        "DG",
+        "DT", 
+        "UNK",
+        "A", 
+        "C", 
+        "G", 
+        "U", 
+        "DX",
+        "X",
+    ]
+
+    expected = [
+        False,
+        False,
+        False,
+        False,
+        False,
+        True,
+        False,
+        True,
+        False,
+        False,
+        True,
+        False,
+        True,
+        False,
+        False,
+        False,
+    ]
+    assert (is_purine(sequence) == np.array(expected)).all()
+
+def test_is_unknown_nucleotide():
+    sequence = [
+        "MET",
+        "LEU",
+        "GLY",
+        "VAL",
+        "ALA",
+        "DA", 
+        "DC", 
+        "DG",
+        "DT", 
+        "UNK",
+        "A", 
+        "C", 
+        "G", 
+        "U", 
+        "DX",
+        "X",
+    ]
+
+    expected = [
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        True,
+        True,
+    ]
+
+    assert (is_unknown_nucleotide(sequence) == np.array(expected)).all()
+
