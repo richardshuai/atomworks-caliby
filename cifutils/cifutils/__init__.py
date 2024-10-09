dbef1c4921f5d6450115f1af5da8b180a526f1e6
Simon Mathis <smathis@uw.edu>
2024-10-09 21:14:50 +0000

    fix: patch biotite's get_residue_starts function to differentiate between residues of different transformation ids

--- cifutils/cifutils/__init__.py
+++ cifutils/cifutils/__init__.py
@@ -6,3 +6,9 @@ from cifutils.parser import CIFParser  # noqa: F401
 logger = logging.getLogger("cifutils")
 _log_level = os.environ.get("CIFUTILS_LOG_LEVEL", "WARNING").upper()
 logger.setLevel(_log_level)
+
+# Monkey patch biotite
+import biotite.structure as struc  # noqa: E402
+from cifutils.utils.selection_utils import get_residue_starts  # noqa: E402
+
+struc.get_residue_starts = get_residue_starts
--- cifutils/cifutils/utils/selection_utils.py
+++ cifutils/cifutils/utils/selection_utils.py
@@ -43,3 +43,20 @@ def annot_start_stop_idxs(atom_array: AtomArray, annots: str | list[str], add_ex
     if add_exclusive_stop:
         return np.concatenate(([0], start_stop_idxs, [len(atom_array)]))
     return np.concatenate(([0], start_stop_idxs))
+
+
+def get_residue_starts(atom_array: AtomArray, add_exclusive_stop: bool = False):
+    """
+    More robust version of `biotite.structure.residues.get_residue_starts` that also
+    differentiates between residues resulting from different transformation ids.
+
+    Backwards compatible with `biotite.structure.residues.get_residue_starts` if the
+    `transformation_id` annotation is not present.
+
+    References:
+        - https://github.com/biotite-dev/biotite/blob/231eefed334e1d3509c1b7cb3f2bfd71d4b0eeb0/src/biotite/structure/residues.py#L35
+    """
+    _annots_to_check = ["chain_id", "res_name", "res_id", "ins_code", "transformation_id"]
+    existing_annots = atom_array.get_annotation_categories()
+    annots_to_check = [annot for annot in _annots_to_check if annot in existing_annots]
+    return annot_start_stop_idxs(atom_array, annots=annots_to_check, add_exclusive_stop=add_exclusive_stop)
