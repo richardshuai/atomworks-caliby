c19b8f3c4f683a35eb112118812088854a5fbe5b
ncorley <ncorley@uw.edu>
2024-10-31 16:40:00 -0700

    chore: tests

---
+++ datahub/tests/utils/test_io.py
@@ -0,0 +1,41 @@
+import pickle
+from pathlib import Path
+
+import pytest
+from assertpy import assert_that
+from biotite.structure import AtomArray
+
+from datahub.utils.io import convert_af3_model_output_to_atom_array
+
+# NOTE: Not the "true" model outputs; slightly pre-processed for storage efficiency
+TEST_PICKLED_AF3_MODEL_OUTPUTS = ["af3_model_outs_protein_only.pkl"]
+
+TEST_DATA_PATH = Path(__file__).resolve().parents[1] / "data"
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("file_path", TEST_PICKLED_AF3_MODEL_OUTPUTS)
+def test_convert_af3_model_output_to_atom_array(file_path):
+    full_path = TEST_DATA_PATH / file_path
+
+    # Load the model outputs
+    with open(full_path, "rb") as f:
+        model_outputs = pickle.load(f)
+
+    # Convert the model outputs to an AtomArray
+    atom_array = convert_af3_model_output_to_atom_array(
+        model_outputs["atom_to_token_map"],
+        model_outputs["chain_iids"],
+        model_outputs["decoded_restypes"],
+        model_outputs["xyz"],
+        model_outputs["elements"],
+        model_outputs["token_is_atomized"],
+    )
+
+    # Smoke tests: Check if the AtomArray has the correct shape
+    assert_that(atom_array).is_instance_of(AtomArray)
+    assert_that(atom_array).is_length(len(model_outputs["xyz"]))
+
+
+if __name__ == "__main__":
+    pytest.main(["-s", "-v", "-m not very_slow", __file__])
