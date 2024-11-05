4372dcd4af1f859e014c7c7b3a76524056daaa2e
ncorley <ncorley@uw.edu>
2024-11-05 10:03:48 -0800

    refactor: make covalent modification transform functional

--- datahub/datahub/transforms/covalent_modifications.py
+++ datahub/datahub/transforms/covalent_modifications.py
@@ -1,4 +1,4 @@
-"""Transforms to handle covalent modificaitons"""
+"""Transforms to handle covalent modifications"""
 
 from __future__ import annotations
 
@@ -13,6 +13,65 @@ from datahub.transforms._checks import (
 )
 from datahub.transforms.atomize import AtomizeByCCDName
 from datahub.transforms.base import Transform
+
+
+def flag_and_reassign_covalent_modifications(atom_array: AtomArray) -> AtomArray:
+    """
+    Mark covalent modifications for atomization and reassign the corresponding 
+    PN unit annotations. 
+
+    Args:
+        atom_array (AtomArray): Current `AtomArray` within the Transform pipeline
+
+    Returns:
+        AtomArray: The modified `AtomArray` with updated annotations for covalent
+            modifications. The `pn_unit_id` and `pn_unit_iid` of polymer atoms are
+            reassigned to those of the non-polymer unit they are bound to, and the
+            `atomize` annotation is set to `True` for these atoms.
+
+    NOTE: If `atomize` annotation is not present in the `AtomArray`, it will be added.
+    """
+    # Get all inter-PN unit bonds in the entry (i.e. between a polymer and a non-polymer PN unit)
+    inter_pn_unit_bond_mask = get_inter_pn_unit_bond_mask(atom_array)
+    bonds_to_check = atom_array.bonds.as_array()[inter_pn_unit_bond_mask]
+
+    # Filter out bonds that are not between a polymer and a non-polymer PN unit
+    bonds_to_check = bonds_to_check[
+        # One atom is a polymer, the other is not => must be polymer/non-polymer bond
+        atom_array.is_polymer[bonds_to_check[:, 0]] != atom_array.is_polymer[bonds_to_check[:, 1]]
+    ]
+
+    # Add the atomize annotation to the AtomArray, if not already present
+    if "atomize" not in atom_array.get_annotation_categories():
+        atom_array.set_annotation("atomize", np.array([False] * len(atom_array)))
+
+    # Loop through inter-molecular bonds
+    # NOTE: There aren't likely to be many inter-molecular bonds in the entry, so vectorization is not necessary and would be less readable
+    for bond in bonds_to_check:
+        # Get the atoms involved in the inter-molecular bonds
+        atom_a = atom_array[bond[0]]
+        atom_b = atom_array[bond[1]]
+
+        # Note which atom is in the polymer and which is in the non-polymer
+        polymer_atom, non_polymer_atom = (atom_a, atom_b) if atom_a.is_polymer else (atom_b, atom_a)
+
+        # Create a mask of the atoms in the residue that is covalently bound to the non-polymer PN unit
+        # We can uniquely identify a residue by its res_id, pn_unit_iid, and chain_id (or chain_iid, either works)
+        polymer_residue_mask = (
+            (atom_array.res_id == polymer_atom.res_id)
+            & (atom_array.chain_id == polymer_atom.chain_id)
+            & (atom_array.pn_unit_iid == polymer_atom.pn_unit_iid)
+        )
+
+        # For all atoms in the target polymer residue, set the pn_unit_iid and the pn_unit_id to that of the non-polymer PN unit
+        num_residues = np.sum(polymer_residue_mask)
+        atom_array.pn_unit_id[polymer_residue_mask] = np.array([non_polymer_atom.pn_unit_id] * num_residues)
+        atom_array.pn_unit_iid[polymer_residue_mask] = np.array([non_polymer_atom.pn_unit_iid] * num_residues)
+
+        # Mark the non-polymer residue for atomization (now includes all atoms in the bonded polymer residue)
+        atom_array.atomize[(atom_array.pn_unit_iid == non_polymer_atom.pn_unit_iid)] = True
+
+        return atom_array
 
 
 class FlagAndReassignCovalentModifications(Transform):
@@ -36,48 +95,5 @@ class FlagAndReassignCovalentModifications(Transform):
         check_atom_array_annotation(data, ["pn_unit_id", "pn_unit_iid"])
 
     def forward(self, data: dict) -> dict:
-        atom_array = data["atom_array"]
-
-        # Get all inter-PN unit bonds in the entry (i.e. between a polymer and a non-polymer PN unit)
-        inter_pn_unit_bond_mask = get_inter_pn_unit_bond_mask(atom_array)
-        bonds_to_check = atom_array.bonds.as_array()[inter_pn_unit_bond_mask]
-
-        # Filter out bonds that are not between a polymer and a non-polymer PN unit
-        bonds_to_check = bonds_to_check[
-            # One atom is a polymer, the other is not => must be polymer/non-polymer bond
-            atom_array.is_polymer[bonds_to_check[:, 0]] != atom_array.is_polymer[bonds_to_check[:, 1]]
-        ]
-
-        # Add the atomize annotation to the AtomArray, if not already present
-        if "atomize" not in atom_array.get_annotation_categories():
-            atom_array.set_annotation("atomize", np.array([False] * len(atom_array)))
-
-        # Loop through inter-molecular bonds
-        # NOTE: There aren't likely to be many inter-molecular bonds in the entry, so vectorization is not necessary and would be less readable
-        for bond in bonds_to_check:
-            # Get the atoms involved in the inter-molecular bonds
-            atom_a = atom_array[bond[0]]
-            atom_b = atom_array[bond[1]]
-
-            # Note which atom is in the polymer and which is in the non-polymer
-            polymer_atom, non_polymer_atom = (atom_a, atom_b) if atom_a.is_polymer else (atom_b, atom_a)
-
-            # Create a mask of the atoms in the residue that is covalently bound to the non-polymer PN unit
-            # We can uniquely identify a residue by its res_id, pn_unit_iid, and chain_id (or chain_iid, either works)
-            polymer_residue_mask = (
-                (atom_array.res_id == polymer_atom.res_id)
-                & (atom_array.chain_id == polymer_atom.chain_id)
-                & (atom_array.pn_unit_iid == polymer_atom.pn_unit_iid)
-            )
-
-            # For all atoms in the target polymer residue, set the pn_unit_iid and the pn_unit_id to that of the non-polymer PN unit
-            num_residues = np.sum(polymer_residue_mask)
-            atom_array.pn_unit_id[polymer_residue_mask] = np.array([non_polymer_atom.pn_unit_id] * num_residues)
-            atom_array.pn_unit_iid[polymer_residue_mask] = np.array([non_polymer_atom.pn_unit_iid] * num_residues)
-
-            # Mark the non-polymer residue for atomization (now includes all atoms in the bonded polymer residue)
-            atom_array.atomize[(atom_array.pn_unit_iid == non_polymer_atom.pn_unit_iid)] = True
-
-        # Update and return
-        data["atom_array"] = atom_array
+        data["atom_array"] = flag_and_reassign_covalent_modifications(data["atom_array"])
         return data
