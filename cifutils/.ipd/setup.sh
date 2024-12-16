430ae711a7152ea3a25b6136ddd9af563db39851
Simon Mathis <simon.mathis@gmail.com>
2024-12-16 07:15:15 -0800

    fix: add links to the ccd & pdb mirrors

--- cifutils/.ipd/setup.sh
+++ cifutils/.ipd/setup.sh
@@ -24,7 +24,7 @@ echo "Adding CCD MIRROR to your environment variables"
 if ! grep -q "CCD_MIRROR_PATH" .env; then
     echo "... setting CCD_MIRROR_PATH in your .env file"
     echo "" >> .env
-    echo "CCD_MIRROR_PATH=$CCD_MIRROR_PATH" >> .env
+    echo "CCD_MIRROR_PATH=$(readlink -f .ipd/ccd)" >> .env
 else
     echo "... CCD_MIRROR_PATH already set in your .env file"
 fi
@@ -34,12 +34,11 @@ echo "Adding PDB MIRROR to your environment variables"
 if ! grep -q "PDB_MIRROR_PATH" .env; then
     echo "... setting PDB_MIRROR_PATH in your .env file"
     echo "" >> .env
-    echo "PDB_MIRROR_PATH=$PDB_MIRROR_PATH" >> .env
+    echo "PDB_MIRROR_PATH=$(readlink -f .ipd/pdb)" >> .env
 else
     echo "... PDB_MIRROR_PATH already set in your .env file"
 fi
 
-echo
 echo "Your .env file is:"
 echo "================================================"
 cat .env
