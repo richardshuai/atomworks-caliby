fix: include nucleic acids when masking residues with unresolved back… (#96)
* fix: include nucleic acids when masking residues with unresolved backbones

* fix: templates

datahub/src/datahub/transforms/featurize_unresolved_residues.py
@@ -21,23 +21,39 @@ from datahub.utils.token import (
 )
 
 
-def mask_residues_with_unresolved_backbone_atoms(atom_array: AtomArray) -> AtomArray:
+def mask_polymer_residues_with_unresolved_frame_atoms(atom_array: AtomArray) -> AtomArray:
     """If a polymer residue has an unresolved backbone atom (occupancy == 0), set the occupancy of the entire residue to zero."""
-    backbone_atom_names = ["N", "CA", "C"]
+
+    # > "Protein tokens use their residue's backbone (N, CA, C) while DNA and RNA tokens use (C1', C3', C4') [to construct frame]"
+    protein_backbone_atom_names = ["N", "CA", "C"]
+    nucleic_acid_backbone_atom_names = ["C1'", "C3'", "C4'"]
 
     # ... subset to backbone atoms within polymers with unresolved coordinates
     # (We treat partially occupied atoms as occupied; e.g., those resolved from "altlocs")
-    protein_mask = (
-        np.isin(atom_array.chain_type, ChainTypeInfo.PROTEINS) if "chain_type" in atom_array else atom_array.is_polymer
-    )
-    unresolved_polymer_backbone_mask = (
-        protein_mask & np.isin(atom_array.atom_name, backbone_atom_names) & (atom_array.occupancy == 0)
-    )
+    if "chain_type" in atom_array.get_annotation_categories():
+        # ... proteins
+        protein_mask = np.isin(atom_array.chain_type, ChainTypeInfo.PROTEINS)
+        unresolved_backbone_mask = (
+            protein_mask & np.isin(atom_array.atom_name, protein_backbone_atom_names) & (atom_array.occupancy == 0)
+        )
 
-    # ... get the residue-wise mask for unresolved backbone atoms
-    unresolved_backbone_res_mask = apply_and_spread_residue_wise(
-        atom_array, unresolved_polymer_backbone_mask, function=np.any
-    )
+        # ... nucleic acids
+        nucleic_acid_mask = np.isin(atom_array.chain_type, ChainTypeInfo.NUCLEIC_ACIDS)
+        unresolved_backbone_mask |= (
+            nucleic_acid_mask
+            & np.isin(atom_array.atom_name, nucleic_acid_backbone_atom_names)
+            & (atom_array.occupancy == 0)
+        )
+    else:
+        # ... rely on atom names if chain type is not available
+        unresolved_backbone_mask = (
+            np.isin(atom_array.atom_name, protein_backbone_atom_names + nucleic_acid_backbone_atom_names)
+            & (atom_array.occupancy == 0)
+            & atom_array.is_polymer
+        )
+
+    # Residue-wise mask for unresolved backbone atoms
+    unresolved_backbone_res_mask = apply_and_spread_residue_wise(atom_array, unresolved_backbone_mask, function=np.any)
 
     # ... mask the occupancy of the entire residue
     atom_array.occupancy[unresolved_backbone_res_mask] = 0
@@ -45,14 +61,19 @@ def mask_residues_with_unresolved_backbone_atoms(atom_array: AtomArray) -> AtomA
     return atom_array
 
 
-class MaskResiduesWithUnresolvedBackboneAtoms(Transform):
-    """For residues with at least one unresolved backbone atom, mask (set to occupancy zero) the entire residue.
+class MaskPolymerResiduesWithUnresolvedFrameAtoms(Transform):
+    """For residues with at least one unresolved frame atom, mask (set to occupancy zero) the entire residue.
 
-    If we don't have backbone atoms, then:
-        - We cannot build backbone frames
+    If we don't have frame atoms, then:
+        - We cannot build residue frames
         - The local structure quality is likely poor
 
-    As an example, see PDB ID `6Z3R`, which has unresolved C and CA atoms.
+    We (and AF-3) consider the frame atoms to be:
+        - Proteins: N, CA, C
+        - Nucleic Acids: C1', C3', C4'
+
+    As an example for proteins, see PDB ID `6Z3R`, which has unresolved C and CA atoms.
+    As an example fo nucleic acids, see 7Z24, which has unresolved C1', C2', and C3' (but does have a resolved oxygen)
 
     NOTE: This transform must be applied before other transform that rely on the `occupancy` annotation.
     """
@@ -67,10 +88,10 @@ class MaskResiduesWithUnresolvedBackboneAtoms(Transform):
     def check_input(self, data: dict[str, Any]) -> None:
         check_contains_keys(data, ["atom_array"])
         check_is_instance(data, "atom_array", AtomArray)
-        check_atom_array_annotation(data, ["occupancy"])
+        check_atom_array_annotation(data, ["occupancy", "chain_type"])
 
     def forward(self, data: dict[str, Any]) -> dict[str, Any]:
-        data["atom_array"] = mask_residues_with_unresolved_backbone_atoms(data["atom_array"])
+        data["atom_array"] = mask_polymer_residues_with_unresolved_frame_atoms(data["atom_array"])
         return data
 
 
@@ -272,7 +293,7 @@ class PlaceUnresolvedTokenAtomsOnRepresentativeAtom(Transform):
         or "coord_to_be_noised" (if we want to modify only the coordinates that will be noised).
     """
 
-    requires_previous_transforms = ["MaskResiduesWithUnresolvedBackboneAtoms", "AtomizeByCCDName"]
+    requires_previous_transforms = ["MaskPolymerResiduesWithUnresolvedFrameAtoms", "AtomizeByCCDName"]
 
     def __init__(self, annotation_to_update: str = "coord_to_be_noised") -> None:
         self.annotation_to_update = annotation_to_update

datahub/tests/transforms/test_featurize_unresolved_residues.py
@@ -10,10 +10,10 @@ from datahub.transforms.atomize import AtomizeByCCDName, FlagNonPolymersForAtomi
 from datahub.transforms.base import Compose
 from datahub.transforms.encoding import EncodeAtomArray
 from datahub.transforms.featurize_unresolved_residues import (
-    MaskResiduesWithUnresolvedBackboneAtoms,
+    MaskPolymerResiduesWithUnresolvedFrameAtoms,
     PlaceUnresolvedTokenAtomsOnRepresentativeAtom,
     PlaceUnresolvedTokenOnClosestResolvedTokenInSequence,
-    mask_residues_with_unresolved_backbone_atoms,
+    mask_polymer_residues_with_unresolved_frame_atoms,
 )
 from datahub.transforms.filters import RemoveUnresolvedPNUnits
 from datahub.utils.testing import cached_parse
@@ -39,7 +39,7 @@ def test_mask_residues_with_unresolved_backbone_atoms(pdb_id):
     changed_atom = atom_array[resolved_ca_atoms][0]
 
     # ... apply the transform
-    updated_atom_array = mask_residues_with_unresolved_backbone_atoms(atom_array)
+    updated_atom_array = mask_polymer_residues_with_unresolved_frame_atoms(atom_array)
 
     # ... assert that the manually set CA atom's residue is masked
     changed_residue_mask = (updated_atom_array.chain_id == changed_atom.chain_id) & (
@@ -52,7 +52,7 @@ def test_mask_residues_with_unresolved_backbone_atoms(pdb_id):
     assert np.all(updated_atom_array.occupancy[unchanged_residue_mask] == atom_array.occupancy[unchanged_residue_mask])
 
 
-FEATURIZE_UNRESOLVED_RESIDUES_TEST_CASES = ["6wtf", "7rcu", "8e83", "7okl"]
+FEATURIZE_UNRESOLVED_RESIDUES_TEST_CASES = ["6wtf", "7rcu", "8e83", "7okl", "7z24"]
 
 
 @pytest.mark.parametrize("pdb_id", FEATURIZE_UNRESOLVED_RESIDUES_TEST_CASES)
@@ -73,7 +73,7 @@ def test_place_unresolved_token_atoms_on_representative_atom(pdb_id):
     pipe = Compose(
         [
             FlagNonPolymersForAtomization(),
-            MaskResiduesWithUnresolvedBackboneAtoms(),
+            MaskPolymerResiduesWithUnresolvedFrameAtoms(),
             AtomizeByCCDName(atomize_by_default=True, res_names_to_ignore=encoding.tokens),
             CopyAnnotation("coord", "coord_to_be_noised"),
             EncodeAtomArray(encoding),
@@ -139,7 +139,7 @@ def test_place_unresolved_token_on_closest_resolved_token_in_sequence(pdb_id):
         [
             FlagNonPolymersForAtomization(),
             RemoveUnresolvedPNUnits(),
-            MaskResiduesWithUnresolvedBackboneAtoms(),
+            MaskPolymerResiduesWithUnresolvedFrameAtoms(),
             AtomizeByCCDName(atomize_by_default=True, res_names_to_ignore=encoding.tokens),
             EncodeAtomArray(encoding),
             CopyAnnotation("coord", "coord_to_be_noised"),