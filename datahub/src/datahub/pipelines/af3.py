dc78b580863a21447d49f5fd8aaf796a2f5b31a8
Arnav Nagle <43835955+ArnNag@users.noreply.github.com>
2025-07-31 14:56:53 -0700

    refactor: update af3 pipeline to match modelhub and implement regression test (#161)

--- datahub/src/datahub/pipelines/af3.py
+++ datahub/src/datahub/pipelines/af3.py
@@ -3,7 +3,13 @@ from pathlib import Path
 
 import numpy as np
 import torch
-from cifutils.constants import AF3_EXCLUDED_LIGANDS, GAP, STANDARD_AA, STANDARD_DNA, STANDARD_RNA
+from cifutils.constants import (
+    AF3_EXCLUDED_LIGANDS,
+    GAP,
+    STANDARD_AA,
+    STANDARD_DNA,
+    STANDARD_RNA,
+)
 from cifutils.enums import ChainType
 
 from datahub.common import exists
@@ -32,12 +38,17 @@ from datahub.transforms.base import (
     RandomRoute,
     SubsetToKeys,
 )
+from datahub.transforms.bfactor_conditioned_transforms import SetOccToZeroOnBfactor
 from datahub.transforms.bonds import AddAF3TokenBondFeatures
 from datahub.transforms.center_random_augmentation import CenterRandomAugmentation
 from datahub.transforms.chirals import AddAF3ChiralFeatures
-from datahub.transforms.covalent_modifications import FlagAndReassignCovalentModifications
+from datahub.transforms.covalent_modifications import (
+    FlagAndReassignCovalentModifications,
+)
 from datahub.transforms.crop import CropContiguousLikeAF3, CropSpatialLikeAF3
-from datahub.transforms.diffusion.batch_structures import BatchStructuresForDiffusionNoising
+from datahub.transforms.diffusion.batch_structures import (
+    BatchStructuresForDiffusionNoising,
+)
 from datahub.transforms.diffusion.edm import SampleEDMNoise
 from datahub.transforms.dna.pad_dna import PadDNA
 from datahub.transforms.encoding import EncodeAF3TokenLevelFeatures, EncodeAtomArray
@@ -93,7 +104,7 @@ def build_af3_transform_pipeline(
     # Undesired res names
     undesired_res_names: list[str] = AF3_EXCLUDED_LIGANDS,
     # Conformer generation params
-    conformer_generation_timeout: float | tuple[float, float] = (3.0, 0.15),  # seconds
+    conformer_generation_timeout: float = 5.0,  # seconds
     use_element_for_atom_names_of_atomized_tokens: bool = False,
     # Template params
     max_n_template: int = 20,  # Maximum number of templates to return from our template search (distinct from n_template)
@@ -101,7 +112,10 @@ def build_af3_transform_pipeline(
     template_max_seq_similarity: float = 60.0,
     template_min_seq_similarity: float = 10.0,
     template_min_length: int = 10,
-    template_allowed_chain_types: list[ChainType] = [ChainType.POLYPEPTIDE_L, ChainType.RNA],
+    template_allowed_chain_types: list[ChainType] = [
+        ChainType.POLYPEPTIDE_L,
+        ChainType.RNA,
+    ],
     template_distogram_bins: torch.Tensor = torch.linspace(3.25, 50.75, 38),
     template_default_token: str = GAP,
     # MSA parameters
@@ -117,6 +131,8 @@ def build_af3_transform_pipeline(
     return_atom_array: bool = True,
     # DNA
     pad_dna_p_skip: float = 0.0,
+    b_factor_min: float | None = None,
+    b_factor_max: float | None = None,
 ):
     """Build the AF3 pipeline with specified parameters.
 
@@ -126,16 +142,15 @@ def build_af3_transform_pipeline(
     and generating reference molecule features.
 
     Args:
-        crop_size: The size of the crop. Defaults to 384.
-        crop_center_cutoff_distance: The cutoff distance for spatial cropping.
+        crop_size (int, optional): The size of the crop. Defaults to 384.
+        crop_center_cutoff_distance (float, optional): The cutoff distance for spatial cropping.
             Defaults to 15.0.
-        crop_contiguous_probability: The probability of using contiguous cropping.
+        crop_contiguous_probability (float, optional): The probability of using contiguous cropping.
             Defaults to 0.5.
-        crop_spatial_probability: The probability of using spatial cropping.
+        crop_spatial_probability (float, optional): The probability of using spatial cropping.
             Defaults to 0.5.
-        conformer_generation_timeout: The timeout for conformer generation in seconds.
-            Defaults to (3.0, 0.15), which gives a timeout of 3.0 + 0.15 * (n_conformers - 1)
-            seconds per unique CCD code.
+        conformer_generation_timeout (float, optional): The timeout for conformer generation in seconds.
+            Defaults to 10.0.
 
     Returns:
         Transform: A composed pipeline of transforms.
@@ -171,6 +186,7 @@ def build_af3_transform_pipeline(
             extra_info_key_with_pn_unit_iids_to_keep="all_pn_unit_iids_after_processing"
         ),  # Filter to non-clashing PN units
         RemoveTerminalOxygen(),
+        SetOccToZeroOnBfactor(b_factor_min, b_factor_max),
         RemoveUnresolvedPNUnits(),
         RemovePolymersWithTooFewResolvedResidues(min_residues=4),
         MaskPolymerResiduesWithUnresolvedFrameAtoms(),
@@ -183,7 +199,6 @@ def build_af3_transform_pipeline(
                 False: PadDNA(p_skip=pad_dna_p_skip),
             },
         ),
-        FlagAndReassignCovalentModifications(),
         FlagAndReassignCovalentModifications(),
         FlagNonPolymersForAtomization(),
         AddGlobalAtomIdAnnotation(),
@@ -201,22 +216,24 @@ def build_af3_transform_pipeline(
     # Crop
 
     # ... crop around our query pn_unit(s) early, since we don't need the full structure moving forward
-    cropping_transform = RandomRoute(
-        transforms=[
-            CropContiguousLikeAF3(
-                crop_size=crop_size,
-                keep_uncropped_atom_array=True,
-                max_atoms_in_crop=max_atoms_in_crop,
-            ),
-            CropSpatialLikeAF3(
-                crop_size=crop_size,
-                crop_center_cutoff_distance=crop_center_cutoff_distance,
-                keep_uncropped_atom_array=True,
-                max_atoms_in_crop=max_atoms_in_crop,
-            ),
-        ],
-        probs=[crop_contiguous_probability, crop_spatial_probability],
-    )
+    cropping_transform = Identity()
+    if crop_size is not None:
+        cropping_transform = RandomRoute(
+            transforms=[
+                CropContiguousLikeAF3(
+                    crop_size=crop_size,
+                    keep_uncropped_atom_array=True,
+                    max_atoms_in_crop=max_atoms_in_crop,
+                ),
+                CropSpatialLikeAF3(
+                    crop_size=crop_size,
+                    crop_center_cutoff_distance=crop_center_cutoff_distance,
+                    keep_uncropped_atom_array=True,
+                    max_atoms_in_crop=max_atoms_in_crop,
+                ),
+            ],
+            probs=[crop_contiguous_probability, crop_spatial_probability],
+        )
 
     transforms.append(
         ConditionalRoute(
@@ -242,6 +259,7 @@ def build_af3_transform_pipeline(
             RandomSubsampleTemplates(n_template=n_template),
         ]
     )
+
     inference_template_loading_from_disk = AddRFTemplates(
         max_n_template=n_template,  # return at most n_template (e.g., 4 in AF-3) from our template search (no subsampling)
         pick_top=True,
@@ -259,7 +277,6 @@ def build_af3_transform_pipeline(
     )
 
     transforms += [
-        # Post-crop annotations
         AddGlobalTokenIdAnnotation(),  # required for reference molecule features and TokenToAtomMap
         EncodeAF3TokenLevelFeatures(sequence_encoding=af3_sequence_encoding),
         GetAF3ReferenceMoleculeFeatures(
@@ -296,7 +313,10 @@ def build_af3_transform_pipeline(
         ),
         PairAndMergePolymerMSAs(dense=dense_msa),
         # ... encode MSA to AF-3 format
-        EncodeMSA(encoding=af3_sequence_encoding, token_to_use_for_gap=af3_sequence_encoding.token_to_idx["<G>"]),
+        EncodeMSA(
+            encoding=af3_sequence_encoding,
+            token_to_use_for_gap=af3_sequence_encoding.token_to_idx["<G>"],
+        ),
         # ... fill MSA, indexing into only the portions of the polymers that are present in the cropped structure
         FillFullMSAFromEncoded(pad_token=af3_sequence_encoding.token_to_idx["<G>"]),
         AddAF3TokenBondFeatures(),
@@ -319,7 +339,8 @@ def build_af3_transform_pipeline(
         # ... handling of unresolved residues (note that these Transforms create the "atom_array_to_noise" dictionary, if not already present)
         PlaceUnresolvedTokenAtomsOnRepresentativeAtom(annotation_to_update="coord_to_be_noised"),
         PlaceUnresolvedTokenOnClosestResolvedTokenInSequence(
-            annotation_to_update="coord_to_be_noised", annotation_to_copy="coord_to_be_noised"
+            annotation_to_update="coord_to_be_noised",
+            annotation_to_copy="coord_to_be_noised",
         ),
         # Feature aggregation
         AggregateFeaturesLikeAF3(),
