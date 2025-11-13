7e12a8a11bf810e21a2f52342d2e79e5ec085c22
ncorley <ncorley@uw.edu>
2025-11-13 12:42:52 -0800

    fix: remove ASE import so we dont introduce a dependency

--- src/atomworks/ml/datasets/__init__.py
+++ src/atomworks/ml/datasets/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
-# Re-export all dataset classes for backward compatibility
-from .ase_dataset import AseDBDataset
+# HACK: Re-export all dataset classes for backward compatibility
+# In the future, we will just import from their respective modules (e.g., from .pandas_dataset import PandasDataset)
 from .base import ExampleIDMixin, MolecularDataset
 from .concat_dataset import ConcatDatasetWithID, FallbackDatasetWrapper, get_row_and_index_by_example_id
 from .file_dataset import FileDataset
@@ -10,7 +10,6 @@ from .pandas_dataset import PandasDataset, StructuralDatasetWrapper
 logger = logging.getLogger("datasets")
 
 __all__ = [
-    "AseDBDataset",
     "ConcatDatasetWithID",
     "ExampleIDMixin",
     "FallbackDatasetWrapper",
